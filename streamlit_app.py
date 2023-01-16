c2f8e2a715accb593c079c67ebbef73975afe3c7
TinyTalent10 <122782564+TinyTalent10@users.noreply.github.com>
2023-01-16 15:07:39 +0530

    Update streamlit_app.py
    added texts for page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
+
 streamlit.title('My Parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal ')
+streamlit.text('Kale spinach cucumber smoothie')
+streamlit.text('Hard boiled egg')
 
