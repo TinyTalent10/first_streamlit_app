ca9dbeb18caa1619df27bc75c223f3c9b3f4763e
TinyTalent10 <122782564+TinyTalent10@users.noreply.github.com>
2023-01-16 17:14:51 +0530

    Update streamlit_app.py
    Added 4th item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit
 
-streamlit.title('My Parents new healthy diner')
+streamlit.title('Breakfast Favorites')
 
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal ')
 streamlit.text('Kale spinach cucumber smoothie')
 streamlit.text('Hard boiled egg')
+streamlit.text('Avocado Toast ')
 
